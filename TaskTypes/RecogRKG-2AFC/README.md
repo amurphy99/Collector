ed04ad853f7412c2a03013d5dc7a12e74dab4f7d
amurphy99 <amurphy62299@gmail.com>
2026-01-16 11:30:50 -0600

    new task to show the answer they selected

---
+++ TaskTypes/RecogRKG-2AFC/README.md
@@ -0,0 +1,5 @@
+# RecogRKG-2AFC
+
+Make the RKG trial display the users actual trial 1 response rather than always showing the right or wrong answer.
+
+
---
+++ TaskTypes/RecogRKG-2AFC/TrialTypes/RKG-2AFC/display.php
@@ -0,0 +1,69 @@
+<?php
+
+// Get their actual response to the first phase of the trial
+$trial_index      = $_SESSION['Position'];
+$trial_1_response = $_SESSION['Trials'  ][$trial_index]['Response']['Response'];
+
+?>
+
+<style>
+    html, body, #content, .main_container {
+        width: 100%;
+        height: 100%;
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+    .main_container {
+        display: table;
+        font-size: 120%;
+    }
+    .main_container > div {
+        display: table-cell;
+        vertical-align: middle;
+        text-align: center;
+    }
+    
+    .cue_container { 
+        margin: 20px;
+        font-size: 200%;
+    }
+    
+    .response_container button {
+        padding: 4x 8px;
+        margin: 10px;
+    }
+</style>
+
+<div class="main_container"><div>
+    <div class="prompt">
+        <b>What was the basis for your decision?</b>
+        <p>Remember: specific study details come to mind</p>
+        <p>Know: familiar, no details</p>
+        <p>Guess: unsure / guessing?</p>
+    </div>
+    <div class="cue_container"><?= $trial_1_response ?></div>
+    <div class="response_container">
+        <button type="button">Remember</button>
+        <button type="button">Know</button>
+        <button type="button">Guess</button>
+    </div>
+</div></div>
+
+<div class="hidden">
+    <input type="hidden" name="Response" id="response">
+    <button class="collectorButton collectorAdvance" id="FormSubmitButton">Next</button>
+</div>
+
+
+<script>
+
+    document.querySelectorAll(".response_container button").forEach(button => {
+        button.addEventListener("click", function() {
+            document.getElementById("response").value = this.innerHTML;
+            document.getElementById("FormSubmitButton").click();
+        });
+    });
+
+</script>
+
